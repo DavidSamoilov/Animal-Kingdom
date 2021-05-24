350dbbfa6441abc83d9e60e57333f6a4dec5fed4
DavidSamoilov <66166375+DavidSamoilov@users.noreply.github.com>
2021-05-24 04:01:38 +0300

    Add files via upload
    Improved CSS
    Modal functions are finished
    
    todo Check for bugs, close to a finished version

--- javaScript/ModalJS/modal.js
+++ javaScript/ModalJS/modal.js
@@ -4,16 +4,35 @@ let sortedInstanceArray = [...InstanceArray].sort(
   (a, b) => a.weight - b.weight
 );
 
-function displaySortedInstanceArrayInModal(array = sortedInstanceArray) {
-  let modalBodyContent = document.querySelector(".modal-list");
-  console.log(modalBodyContent);
-  ulEl = document.createElement("ul");
-  array.forEach((animal, index) => {
-    console.log(animal);
+// Adding the Event Listener to display sorted animals by weight in modal
+let modalButtonEl = document.getElementById("modal-btn");
+modalButtonEl.addEventListener('click',()=>displaySortedInstanceArrayInModal())
+let modalBodyContent = document.getElementById("modal-list");
+
+// Function that Displays all modal details in a list
+function displaySortedInstanceArrayInModal() {
+  const ulEl = document.createElement("ul");
+  ulEl.classList.add('modal-ul')
+  sortedInstanceArray.forEach((item, index) => {
+    console.log(item);
     let animalModalLi = document.createElement("li");
-    animalModalLi.innerText = `${index}.${animal.constructor.name}: ${animal.name}`;
+    animalModalLi.innerText = `${index + 1}. ${
+      item.constructor.name
+    } weight is ${item.weight} Kg`;
 
     ulEl.appendChild(animalModalLi);
   });
   modalBodyContent.appendChild(ulEl);
+  modalButtonEl.removeEventListener('click',()=>displaySortedInstanceArrayInModal())
+
 }
+
+function clearModalText(){
+    modalBodyContent.innerText=''
+
+}
+
+// Clear text in modal when closed
+let modalCloseButtonEl = document.getElementById("modal-close-btn");
+modalCloseButtonEl.addEventListener('click',()=>clearModalText())
+
